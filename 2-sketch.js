ef278df1e45830ddbb4cb911b9a261b68b249048
KIM DONG IL <dongil.kim@powertask.biz>
2022-02-05 22:37:39 +0900

    map

---
+++ 2-sketch.js
@@ -0,0 +1,9 @@
+let arr = new Array(100);
+// let arr = [4, 8, 1, 2, 9];
+arr = arr.fill(0).map(Math.random);
+console.log(arr);
+
+// console.log(arr);
+
+// arr = arr.map((x) => x * 2);
+// console.log(arr);
